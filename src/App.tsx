d6e50f3e0a4c63f6317b4fa5cbc2cd48cb91f191
codejagaban <okpusjamin@gmail.com>
2025-01-11 23:17:46 +0300

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -100,7 +100,6 @@ export function App() {
 
       const imgURL = await framer.getImage();
       if (imgURL?.url) {
-        console.log("Fetched Image URL: ", imgURL.url);
         await removeBg(imgURL.url);
       }
     } else if (framer.mode === "canvas") {
